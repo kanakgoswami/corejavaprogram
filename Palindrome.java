031e363ab4e5478e547c1f054a5007df2728cc61
kanakgoswami <kanak.goswami@yahoo.com>
2022-06-21 19:13:42 -0400

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 public class Palindrome {
     public static void main(String[] args) {
         String str = "Radar",
@@ -10,5 +12,29 @@ public class Palindrome {
         } else {
             System.out.println(str + " is not a Palindrome String.");
         }
+        /////////////////////////////////////////////////////////////////////////////////////////
+        // Check if 'radar' is Palindrome
+        System.out.println("Is radar a Palindrome? - " + isPalindrome("radar")); // true
+        // Check if 'apple' is Palindrome
+        System.out.println("Is apple a Palindrome? - " + isPalindrome("apple")); // false
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        String original = "kanak"; // Objects of String class
+        StringBuffer reverse;
+        reverse = new StringBuffer(original);
+        reverse.reverse();
+        System.out.println(reverse);
+        if (original.equals(reverse.toString()))
+            System.out.println("The string is a palindrome.");
+        else
+            System.out.println("The string isn't a palindrome.");
+    }
+
+    public static boolean isPalindrome(String originalString) {
+
+        String tempString = originalString.replaceAll("\\s+", "").toLowerCase();
+
+        return IntStream.range(0, tempString.length() / 2)
+                .noneMatch(i -> tempString.charAt(i) != tempString.charAt(tempString.length() - i - 1));
+
     }
 }
