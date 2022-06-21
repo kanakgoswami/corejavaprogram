7ae7685ddaef69652ee762d59241924cd320710e
kanakgoswami <kanak.goswami@yahoo.com>
2022-06-21 19:13:38 -0400

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 class EvenOdd {
     public static void main(String args[]) {
         int a[] = { 1, 2, 5, 6, 3, 2 };
@@ -9,5 +14,16 @@ class EvenOdd {
                 System.out.println("Odd   :" + a[i]);
             }
         }
+        //////////////////////////////////////////////////////////////////////////////////
+        List<Integer> numbers = Arrays.asList(1, 4, 8, 40, 11, 22, 33, 99);
+        List<Integer> evenNumbers = numbers.stream().filter(o -> o % 2 == 0).collect(Collectors.toList());
+        System.out.println(evenNumbers);
+        List<Integer> oddNumbers = numbers.stream().filter(o -> o % 2 != 0).collect(Collectors.toList());
+        System.out.println(oddNumbers);
+        /////////////////////////////////////////////////////////////////////////////////
+        List<Integer> numberList = Arrays.asList(1, 4, 8, 40, 11, 22, 33, 99);
+        Predicate<Integer> even = number -> number % 2 == 0;
+        numberList.stream().filter(even.negate()).forEach(System.out::println);
+
     }
 }
